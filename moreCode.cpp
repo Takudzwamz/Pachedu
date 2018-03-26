aeeeca49ee6170c4f0ae1cd48bd9dde5b5541d31
TakuD <saramugomowega@gmail.com>
2018-03-26 22:30:16 +0000

    From the top

--- moreCode.cpp
+++ moreCode.cpp
@@ -1,3 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// Alphabet size (# of symbols)
+#define ALPHABET_SIZE (26)
+ 
+// Converts key current character into index
+// use only 'a' through 'z' and lower case
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
+ 
+// trie node
+struct TrieNode
+{
+    struct TrieNode *children[ALPHABET_SIZE];
+ 
+    // isWordEnd is true if the node represents
+    // end of a word
+    bool isWordEnd;
+};
+ 
+// Returns new trie node (initialized to NULLs)
+struct TrieNode *getNode(void)
+{
+    struct TrieNode *pNode = new TrieNode;
+    pNode->isWordEnd = false;
+ 
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        pNode->children[i] = NULL;
+ 
+    return pNode;
+}
+ 
+// If not present, inserts key into trie.  If the
+// key is prefix of trie node, just marks leaf node
+void insert(struct TrieNode *root,  const string key)
+{
+    struct TrieNode *pCrawl = root;
+ 
+    for (int level = 0; level < key.length(); level++)
+    {
+        int index = CHAR_TO_INDEX(key[level]);
+        if (!pCrawl->children[index])
+            pCrawl->children[index] = getNode();
+ 
+        pCrawl = pCrawl->children[index];
+    }
+ 
+    // mark last node as leaf
+    pCrawl->isWordEnd = true;
+}
+ 
+// Returns true if key presents in trie, else false
+bool search(struct TrieNode *root, const string key)
+{
+    int length = key.length();
+    struct TrieNode *pCrawl = root;
+    for (int level = 0; level < length; level++)
+    {
+        int index = CHAR_TO_INDEX(key[level]);
+ 
+        if (!pCrawl->children[index])
+            return false;
+ 
+        pCrawl = pCrawl->children[index];
+    }
+ 
+    return (pCrawl != NULL && pCrawl->isWordEnd);
+}
+ 
+// Returns 0 if current node has a child
+// If all children are NULL, return 1.
+bool isLastNode(struct TrieNode* root)
+{
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        if (root->children[i])
+            return 0;
+    return 1;
+}
 // print suggestions for given query prefix.
 int printAutoSuggestions(TrieNode* root, const string query)
 {
